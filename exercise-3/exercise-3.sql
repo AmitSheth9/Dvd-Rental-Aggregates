0d97a8e9e164a3040870982d839770576be1ea99
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-04 13:04:38 -0600

    finished the 10 dvd aggregate exercises

--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -1,3 +1,11 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+SELECT country.country, AVG(payment.amount) FROM country
+INNER JOIN city ON city.country_id = country.country_id
+INNER JOIN address ON address.city_id = city.city_id
+INNER JOIN customer ON address.address_id = customer.address_id
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+GROUP BY country.country
+ORDER by avg desc
+LIMIT 10
